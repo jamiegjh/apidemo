aa56eb8392bff779e12d7c0f3cfa6a5ca5396a6b
GaoChang <gaojinghui@gaojinghuideMBP.lan>
2025-11-15 11:10:45 +0800

    首次提交：初始化项目

---
+++ script/test01_image.py
@@ -0,0 +1,13 @@
+# 获取图片验证码
+
+# 导包
+import requests
+
+# 发送请求
+response = requests.get(url="http://kdtx-test.itheima.net/api/captchaImage")
+
+# 查看响应
+print(response.status_code)
+print(response.json().get("uuid"))
+print(response.text)
+
---
+++ script/test02_login.py
@@ -0,0 +1,22 @@
+# 需求：登录成功
+
+# 导包
+import requests
+
+# 发送请求
+url = "http://kdtx-test.itheima.net/api/login"
+header_data = {
+    "Content-Type": "application/json"
+}
+login_data = {
+    "username": "admin",
+    "password": "HM_2023_test",
+    "code": 2,
+    "uuid": "d6ef71b1abb24453ace87022b67f8119"
+}
+response = requests.post(url=url, headers=header_data, json=login_data)
+
+# 查看响应
+print(response.status_code)
+print(response.json())
+
